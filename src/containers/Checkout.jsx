52b880ba00aeaf81898335458f5c7b36f47676d6
cristianiniguez <felizpc1@gmail.com>
2020-12-10 13:32:37 -0400

    estructura: formulario de checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const Checkout = () => <h1>Checkout</h1>;
+import '../styles/components/Checkout.css';
+
+const Checkout = () => (
+  <main className="Checkout">
+    <div className="Checkout__content">
+      <h3>Lista de Pedidos</h3>
+      <div className="Checkout__item">
+        <div className="Checkout__element">
+          <h4>Item name</h4>
+          <span>$10</span>
+        </div>
+        <button type="button">Eliminar</button>
+      </div>
+    </div>
+    <div className="Checkout__sidebar">
+      <h3>Precio total: $10</h3>
+      <button type="button">Continuar pedido</button>
+    </div>
+  </main>
+);
 
 export default Checkout;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,7 +5,7 @@ import Products from '../components/Products';
 import initialState from '../initialState';
 
 const Home = () => (
-  <main>
+  <main className="Home">
     <Products products={initialState.products} />
   </main>
 );
