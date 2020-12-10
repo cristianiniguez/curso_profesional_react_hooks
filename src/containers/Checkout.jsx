e432f7d559d9ca4d1e7d5cd54f3130e2b0c37766
cristianiniguez <felizpc1@gmail.com>
2020-12-10 16:20:30 -0400

    hooks: useContext en la pagina de Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/components/Checkout.css';
 
-const Checkout = () => (
-  <main className="Checkout">
-    <div className="Checkout__content">
-      <h3>Lista de Pedidos</h3>
-      <div className="Checkout__item">
-        <div className="Checkout__element">
-          <h4>Item name</h4>
-          <span>$10</span>
-        </div>
-        <button type="button">
-          <i className="fas fa-trash-alt" />
-        </button>
+import AppContext from '../context/AppContext';
+
+const Checkout = () => {
+  const {
+    state: { cart },
+    removeFromCart,
+  } = useContext(AppContext);
+
+  const handleRemove = removeFromCart;
+
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
+
+  return (
+    <main className="Checkout">
+      <div className="Checkout__content">
+        {cart.length > 0 ? <h3>Lista de Pedidos</h3> : 'Sin pedidos ...'}
+        {cart.map((item) => (
+          <div key={item.cartId} className="Checkout__item">
+            <div className="Checkout__element">
+              <h4>{item.title}</h4>
+              <span>{item.price}</span>
+            </div>
+            <button type="button" onClick={() => handleRemove(item)}>
+              <i className="fas fa-trash-alt" />
+            </button>
+          </div>
+        ))}
       </div>
-    </div>
-    <div className="Checkout__sidebar">
-      <h3>Precio total: $10</h3>
-      <Link to="/checkout/information">
-        <button type="button">Continuar pedido</button>
-      </Link>
-    </div>
-  </main>
-);
+      {cart.length > 0 && (
+        <div className="Checkout__sidebar">
+          <h3>{`Precio total: ${handleSumTotal()}`}</h3>
+          <Link to="/checkout/information">
+            <button type="button">Continuar pedido</button>
+          </Link>
+        </div>
+      )}
+    </main>
+  );
+};
 
 export default Checkout;
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -2,19 +2,21 @@ import { useState } from 'react';
 import initialState from '../initialState';
 
 const useInitialState = () => {
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState({ ...initialState, cartCount: 0 });
 
   const addToCart = (payload) => {
+    const index = state.cartCount;
     setState({
       ...state,
-      cart: [...state.cart, payload],
+      cart: [...state.cart, { ...payload, cartId: index }],
+      cartCount: index + 1,
     });
   };
 
   const removeFromCart = (payload) => {
     setState({
       ...state,
-      cart: state.cart.filter((items) => items.id !== payload.id),
+      cart: state.cart.filter((items) => items.cartId !== payload.cartId),
     });
   };
 
