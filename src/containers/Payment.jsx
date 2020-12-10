2a51f1b84c7286e7d97947bfd3f9a2c8ee99829e
cristianiniguez <felizpc1@gmail.com>
2020-12-10 14:37:02 -0400

    estructura: flujo de pago

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Payment = () => <h1>Payment</h1>;
+import '../styles/components/Payment.css';
+
+const Payment = () => (
+  <main className="Payment">
+    <div className="Payment__content">
+      <h3>Resumen del Pedido:</h3>
+      <div className="Payment__button">Boton de pago con Paypal</div>
+    </div>
+    <div className="Payment__sidebar" />
+  </main>
+);
 
 export default Payment;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Success = () => <h1>Success</h1>;
+import '../styles/components/Success.css';
+
+const Success = () => (
+  <main className="Success">
+    <div className="Success__content">
+      <h2>Felix, Gracias por tu compra</h2>
+      <span>Tu pedido llegará en 3 días a tu dirección</span>
+      <div className="Success__map">Google maps</div>
+    </div>
+  </main>
+);
 
 export default Success;
