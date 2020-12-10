8610ed4244af2aec49b9a1c1949c3f8a4eeee7c0
cristianiniguez <felizpc1@gmail.com>
2020-12-10 14:56:44 -0400

    estructura: integracion de iconos y conexion con react router

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/components/Information.css';
 
@@ -23,7 +24,9 @@ const Information = () => (
       </div>
       <div className="Information__buttons">
         <div className="Information__back">Regresar</div>
-        <div className="Information__next">Pagar</div>
+        <div className="Information__next">
+          <Link to="/checkout/payment">Pagar</Link>
+        </div>
       </div>
     </div>
     <div className="Information__sidebar">
